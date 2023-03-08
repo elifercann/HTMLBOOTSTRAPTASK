8f8433455f8473340edb20f29704f8dc5961fe66
Elif Ercan <ercanelif450@gmail.com>
2023-03-08 19:49:42 +0300

    gönderilen tasktaki tasarım tamamlandı

---
+++ HTMLBOOTSTRAPTASK/Controllers/DenemeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTMLBOOTSTRAPTASK.Controllers
+{
+    public class DenemeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
